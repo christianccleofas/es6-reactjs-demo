e589e247cb3a3415f52de40d888ec13f3746ce7e
Miguel Madregallejo <migsmadregallejo@gmail.com>
2021-05-18 00:13:22 +0800

    Unit test setup

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,9 @@
-import { render, screen } from '@testing-library/react';
+import React from 'react';
+import {shallow} from 'enzyme';
 import App from './App';
+import toJson from 'enzyme-to-json';
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const wrapper = shallow(<App />);
+  expect(wrapper).toMatchSnapshot();
 });
