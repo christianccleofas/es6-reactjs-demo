817d367c58f83fa2b1e5911b73b149bbdf5b99f3
Miguel Madregallejo <migsmadregallejo@gmail.com>
2021-05-24 09:18:19 +0800

    <Item /> Unit test

--- src/components/common/Item/Item.jsx
+++ src/components/common/Item/Item.jsx
@@ -51,11 +51,11 @@ const Item = ({ data, getOrderFromItem, orderList }) => {
 
 	return (
 		<div className="container">
-			{!data.isAvailable ? <span className="floating-text-not-available">Not Available</span> : null}
-			<Paper className={data.isAvailable ? "item-wrapper" : "item-wrapper-not-available"} elevation={3}>
+			{!data.isAvailable ? <span data-testid="span-not-available" className="floating-text-not-available">Not Available</span> : null}
+			<Paper data-testid="paper" className={data.isAvailable ? "item-wrapper" : "item-wrapper-not-available"} elevation={3}>
 				<div className="item-display-wrapper">
 					<img className="item-display-image" src={process.env.PUBLIC_URL + "/assets/images/" + data.image + ".webp"} alt="coffee-test" />
-					{data.discount > 0 ? <div className="item-display-discount">{"-" + data.discount * 100 + "%"}</div> : null}
+					{data.discount > 0 ? <div data-testid="div-display-discount" className="item-display-discount">{"-" + data.discount * 100 + "%"}</div> : null}
 				</div>
 				<div className="details" style={{ display: "flex", alignItems: "center" }}>
 					<div className="item-name">
@@ -63,7 +63,7 @@ const Item = ({ data, getOrderFromItem, orderList }) => {
 					</div>
 					<div className="item-details-wrapper">
 						{data.discount && data.discount > 0 ?
-							<div className="item-details-amount-discounted">
+							<div data-testid="div-amount-discount" className="item-details-amount-discounted">
 								<div className="item-details-amount-n-dsc">
 									{"$" + data.price.toFixed(2)}
 								</div>
@@ -71,16 +71,16 @@ const Item = ({ data, getOrderFromItem, orderList }) => {
 									{"$" + (data.price - (data.price * data.discount)).toFixed(2)}
 								</div>
 							</div> :
-							<div className="item-details-amount">{"$" + data.price.toFixed(2)}</div>}
+							<div data-testid="div-amount" className="item-details-amount">{"$" + data.price.toFixed(2)}</div>}
 					</div>
 				</div>
 				<div className="item-details-addToCart">
 					<div style={{ flex: '1' }}>
-						<IconButton className="test" size="small" onClick={handleDecrease}>
+						<IconButton data-testid="button-subtract" className="test" size="small" onClick={handleDecrease}>
 							<RemoveIcon fontSize="small" />
 						</IconButton>
-						<span className="grp-btn-text">{qty}</span>
-						<IconButton className="test" size="small" onClick={handleIncrease}>
+						<span date-testid="span-qty" className="grp-btn-text">{qty}</span>
+						<IconButton data-testid="button-add" className="test" size="small" onClick={handleIncrease}>
 							<AddIcon fontSize="small" />
 						</IconButton>
 					</div>
--- src/components/common/Item/Item.test.jsx
+++ src/components/common/Item/Item.test.jsx
@@ -12,12 +12,114 @@ const mocks = {
     discount: 0
   },
   getOrderFromItem: jest.fn(),
-  orderList: []
+  orderList: [],
+  qty: 0,
+  useState: jest.fn(),
 }
 
-describe('<Item />: Render Testing', () => {
-  it('Should match snapshot.', () => {
-    const wrapper = shallow(<Item data={mocks.data} getOrderFromItem={mocks.getOrderFromItem} orderList={mocks.orderList} />)
-    expect(wrapper).toMatchSnapshot();
+// jest.mock('react', () => ({
+//   ...jest.requireActual('react'),
+//   useState: x => [x, mocks.useState],
+//   useEffect: f => f()
+// }));
+// const mockUseState = () => jest.spyOn(React, 'useState').mockReturnValueOnce([0, mocks.useState])
+
+describe('<Item />: Component Testing', () => {
+  afterEach(() => {
+    jest.clearAllMocks();
   });
+  describe('<Item />: Conditional render testing', () => {
+    it('Should match snapshot.', () => {
+      const wrapper = shallow(<Item data={mocks.data} getOrderFromItem={mocks.getOrderFromItem} orderList={mocks.orderList} />);
+      expect(wrapper).toMatchSnapshot();
+    });
+    it('Should render with different elemets/styles if data.isAvailable is false.', () => {
+      const data = {
+        id: 0,
+        name: "Test item",
+        isAvailable: false,
+        price: 3.66,
+        image: "8e0b1749cfad49f085e3efff636aef58",
+        discount: 0
+      }
+      const wrapper = shallow(<Item data={data} getOrderFromItem={mocks.getOrderFromItem} orderList={mocks.orderList} />);
+      expect(wrapper.find('[data-testid="span-not-available"]').exists()).toBeTruthy();
+      expect(wrapper.find('[data-testid="paper"]').props().className).toEqual('item-wrapper-not-available');
+    });
+    it('Should render with different elemets/styles if data.isAvailable is true.', () => {
+      const data = {
+        id: 0,
+        name: "Test item",
+        isAvailable: true,
+        price: 3.66,
+        image: "8e0b1749cfad49f085e3efff636aef58",
+        discount: 0
+      }
+      const wrapper = shallow(<Item data={data} getOrderFromItem={mocks.getOrderFromItem} orderList={mocks.orderList} />);
+      expect(wrapper.find('[data-testid="span-not-available"]').exists()).toBeFalsy();
+      expect(wrapper.find('[data-testid="paper"]').props().className).toEqual('item-wrapper');
+    });
+    it('Should render with different elemets/styles if data.discount is not greater than 0.', () => {
+      const data = {
+        id: 0,
+        name: "Test item",
+        isAvailable: false,
+        price: 3.66,
+        image: "8e0b1749cfad49f085e3efff636aef58",
+        discount: 0
+      }
+      const wrapper = shallow(<Item data={data} getOrderFromItem={mocks.getOrderFromItem} orderList={mocks.orderList} />);
+      expect(wrapper.find('[data-testid="div-display-discount"]').exists()).toBeFalsy();
+      expect(wrapper.find('[data-testid="div-amount"]').exists()).toBeTruthy();
+      expect(wrapper.find('[data-testid="div-amount-discount"]').exists()).toBeFalsy();
+    });
+    it('Should render with different elemets/styles if data.discount is greater than 0.', () => {
+      const data = {
+        id: 0,
+        name: "Test item",
+        isAvailable: false,
+        price: 3.66,
+        image: "8e0b1749cfad49f085e3efff636aef58",
+        discount: 5
+      }
+      const wrapper = shallow(<Item data={data} getOrderFromItem={mocks.getOrderFromItem} orderList={mocks.orderList} />);
+      expect(wrapper.find('[data-testid="div-display-discount"]').exists()).toBeTruthy();
+      expect(wrapper.find('[data-testid="div-amount"]').exists()).toBeFalsy();
+      expect(wrapper.find('[data-testid="div-amount-discount"]').exists()).toBeTruthy();
+    });
+  });
+
+  describe('<Item />: Event handler testing without mocked states', () => {
+    test('onClick: button-add should trigger handleIncrease', () => {
+      const wrapper = shallow(<Item data={mocks.data} getOrderFromItem={mocks.getOrderFromItem} orderList={mocks.orderList} />);
+      expect(wrapper.find('[date-testid="span-qty"]').text()).toEqual('0');
+      wrapper.find('[data-testid="button-add"]').simulate('click'); // +1
+      expect(wrapper.find('[date-testid="span-qty"]').text()).toEqual('1');
+    });
+    test('onClick: button-subtract should trigger handleDecrease', () => {
+      const wrapper = shallow(<Item data={mocks.data} getOrderFromItem={mocks.getOrderFromItem} orderList={mocks.orderList} />);
+      expect(wrapper.find('[date-testid="span-qty"]').text()).toEqual('0');
+      wrapper.find('[data-testid="button-add"]').simulate('click'); // +1
+      wrapper.find('[data-testid="button-subtract"]').simulate('click'); // -1
+      expect(wrapper.find('[date-testid="span-qty"]').text()).toEqual('0');
+    });
+  });
+
+  // describe('<Item />: Event handler testing', () => {
+    // beforeEach(() => {
+    //   jest.clearAllMocks()
+    // })
+    // test('onClick: button-add should trigger handleIncrease', () => {
+    //   const wrapper = shallow(<Item data={mocks.data} getOrderFromItem={mocks.getOrderFromItem} orderList={mocks.orderList} />);
+    //   wrapper.find('[data-testid="button-add"]').simulate('click');
+    //   expect(mocks.useState).toHaveBeenCalledWith(1);
+    // });
+    // test('onClick: button-subtract should trigger handleDecrease', () => {
+    //   const wrapper = shallow(<Item data={mocks.data} getOrderFromItem={mocks.getOrderFromItem} orderList={mocks.orderList} />);
+    //   wrapper.find('[data-testid="button-add"]').simulate('click');
+    //   wrapper.find('[data-testid="button-subtract"]').simulate('click');
+    //   console.log(mocks.useState.mock.calls)
+    //   expect(mocks.useState.mock.calls[1].length).toBe(0);
+    // });
+  // });
 });
